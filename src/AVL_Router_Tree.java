bc672b824483bec63fd28ddf2c0ddac9d8fe5876
gkzxy <gabrielkborgessilva@gmail.com>
2026-05-14 04:31:16 -0300

    feat: implementa remocao e balanceamento final da AVL

--- src/AVL_Router_Tree.java
+++ src/AVL_Router_Tree.java
@@ -87,4 +87,65 @@ public class AVL_Router_Tree {
 
         return node;
     }
+
+    private AVLNode minValueNode(AVLNode node) {
+        AVLNode current = node;
+        while (current.getLeft() != null) {
+            current = current.getLeft();
+        }
+        return current;
+    }
+
+    public AVLNode deleteNode(AVLNode node, int id) {
+        if (node == null) {
+            return node;
+        }
+        if (id < node.getRule().getId()) {
+            node.setLeft(deleteNode(node.getLeft(), id));
+        } else if (id > node.getRule().getId()) {
+            node.setRight(deleteNode(node.getRight(), id));
+        } else {
+            if ((node.getLeft() == null) || (node.getRight() == null)) {
+                AVLNode temp = null;
+                if (temp == node.getLeft()) {
+                    temp = node.getRight();
+                } else {
+                    temp = node.getLeft();
+                }
+
+                if ( temp == null){
+                    node = null;
+                } else {
+                    node = temp;
+                }
+            } else {
+                AVLNode temp = minValueNode(node.getRight());
+                node.setRule(temp.getRule());
+                node.setRight(deleteNode(node.getRight(), temp.getRule().getId()));
+            }
+        }
+        if (node == null){
+            return node;
+        }
+        node.setHeight(Math.max(height(node.getLeft()), height(node.getRight())) +1);
+
+        int balance = getBalance(node);
+
+        if (balance > 1 && getBalance(node.getLeft()) >= 0) {
+            return rightRotate(node);
+        }
+        if (balance > 1 && getBalance(node.getLeft()) < 0) {
+            node.setLeft(leftRotate(node.getLeft()));
+            return rightRotate(node);
+        }
+        if (balance < -1 && getBalance(node.getRight()) <= 0) {
+            return leftRotate(node);
+        }
+        if (balance < -1 && getBalance(node.getRight()) > 0) {
+            node.setRight(rightRotate(node.getRight()));
+            return leftRotate(node);
+        }
+
+        return node;
+    }
 }
